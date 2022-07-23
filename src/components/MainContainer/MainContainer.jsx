647a36235948def66f2ae68e10ada3b09f25ffcd
Olena Pohybil <itlentus@gmail.com>
2022-07-23 16:11:03 +0300

    fix height

--- src/components/MainContainer/MainContainer.jsx
+++ src/components/MainContainer/MainContainer.jsx
@@ -6,7 +6,6 @@ const Container = styled.div`
   display: flex;
   padding: 60px 20px 10px;
   overflow: hidden;
-  overflow-y: hidden;
-  max-height: calc(100vh - 20px);
+  /* max-height: calc(100vh - 20px); */
 `;
 export default MainContainer;
